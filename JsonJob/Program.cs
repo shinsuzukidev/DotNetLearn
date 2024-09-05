a4aa676687e569727a3ab8adab2d3fe01f83f73d
shin.suzuki <shin.suzuki.dev@gmail.com>
2024-09-05 23:01:30 +0900

    プロジェクト ファイルを追加します。

---
+++ JsonJob/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace JsonJob
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("__start");
+
+            //----------------------------------------- Dictionary ⇔ json
+            // model to json
+            var dic = new Dictionary<string, string> {
+               { "item1", "aaa"},
+               { "item2", "bbb"},
+               { "item3", "ccc"} 
+            };
+            string json = Newtonsoft.Json.JsonConvert.SerializeObject(dic);
+
+            // json to model
+            var dicModel = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+
+
+            //----------------------------------------- Model ⇔ json
+            var s = new SampleModel {
+                Name = "kato",
+                Age = 30,
+                ItemList = new List<string> { "item1", "item2" }
+            };
+
+            // model to json
+            var jsonSampleModel = Newtonsoft.Json.JsonConvert.SerializeObject(s);  
+
+            // json to model
+            var objSampleModel = Newtonsoft.Json.JsonConvert.DeserializeObject<SampleModel>(jsonSampleModel);
+
+            Console.WriteLine("__end");
+        }
+    }
+}
---
+++ JsonJob/SampleModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonJob
+{
+    public class SampleModel
+    {
+        public string Name { get; set; } = "";
+
+        public int Age { get; set; } = 0;
+
+        public List<string> ItemList { get; set; } = new();
+    }
+}
