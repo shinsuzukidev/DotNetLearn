55cb3eb0a6b97e35ef46d050e628388d6cf91368
shin.suzuki <shin.suzuki.dev@gmail.com>
2024-09-05 23:38:51 +0900

    add setting bind project.

---
+++ SettingBind/AppSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SettingBind
+{
+    public class AppSettings
+    {
+        public string Title { get; set; } = "";
+        public int Revision { get; set; }
+        public bool LoggingEnabled { get; set; }
+        public ConnectionStrings ConnectionStrings { get; set; } = new ConnectionStrings();
+        public HttpStatus[] HttpStatuses { get; set; } = new HttpStatus[] { };
+    }
+}
---
+++ SettingBind/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace SettingBind
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("__start!");
+
+            // appsetting.jsonをモデルにバインド
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            Console.WriteLine(configuration["Title"]);
+            Console.WriteLine(configuration.GetConnectionString("MainDB"));
+
+
+            // モデルにマッピング
+            var appSettings = configuration.Get<AppSettings>();
+            Console.WriteLine(appSettings!.Title);
+            Console.WriteLine(appSettings.ConnectionStrings.MainDB);
+
+
+
+
+
+            Console.WriteLine("__end!");
+        }
+    }
+}
